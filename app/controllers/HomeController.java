e20cced668f35df93b19af706dbdb9aeb52070b7
Live session user <sandeep.k@betsol.com>
2016-10-23 10:19:11 +0000

    done with lambda

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.ConnectionRequest;
 import models.Profile;
 import models.User;
 import play.mvc.Controller;
@@ -48,7 +49,39 @@ public class HomeController extends Controller {
         }).collect(Collectors.toList()))
         );
 
-        return ok();
+        data.set("connectionRequests",objectMapper.valueToTree(user.connectionRequestsReceived.stream().filter(x->x.status.equals(ConnectionRequest.Status.WAITING)).map(connectionRequestRec->{
+
+            ObjectNode connectionReqRecJson = objectMapper.createObjectNode();
+
+            User sendUser = User.find.byId(connectionRequestRec.id);
+            Profile senderProfile = Profile.find.byId(connectionRequestRec.sender.profile.id);
+
+            connectionReqRecJson.put("id",connectionRequestRec.id);
+            connectionReqRecJson.put("firstName",senderProfile.firstName);
+            connectionReqRecJson.put("email",sendUser.email);
+
+            return connectionReqRecJson;
+
+        })));
+
+        data.set("suggestions", objectMapper.valueToTree(User.find.all().stream().filter(x->!user.equals(x)).filter(x->!user.connections.contains(x)).
+                filter(x->!user.connectionRequestsReceived.stream().map(y->y.sender).collect(Collectors.toList()).contains(x)).
+                filter(x->!user.connectionRequestsSent.stream().map(y->y.receiver).collect(Collectors.toList()).contains(x)).
+                map(suggestion->{
+                    ObjectNode suggestionJson = objectMapper.createObjectNode();
+
+                    User suggestUser = User.find.byId(suggestion.id);
+                    Profile suggestProfile = Profile.find.byId(suggestion.profile.id);
+
+                    suggestionJson.put("id",suggestUser.id);
+                    suggestionJson.put("firstName",suggestProfile.firstName);
+
+                    return suggestionJson;
+
+                })
+        ));
+
+        return ok(data);
 
     }
 
